423c0ae43a9a8c265795abfb5d4dcf962db63e9b
Alien-oyi <daijinmin78@gmail.com>
2023-02-24 10:15:15 -0500

    pull

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ $(searcher).on('click', function() {
   const options = {
     method: 'GET',
     headers: { //define options for fetch below
-      // 'X-RapidAPI-Key': '', 
+      // 'X-RapidAPI-Key': '', // needs to be put back in
       'X-RapidAPI-Host': 'concerts-artists-events-tracker.p.rapidapi.com'
     }
   };
@@ -36,11 +36,11 @@ function getShows(artistName) { //when function called, will be passed the artis
       let lat = data.data[i].location.geo.latitude;
       let lon = data.data[i].location.geo.longitude;
 //       //pull the date --> will go in 'date' section of card
-      $('#concert-date').text(data.data[i].startDate);
+      console.log(data.data[i].startDate);
 //       //get band name (could also just use user input result)
-      $('#band-name').text(data.data[i].name);
+      console.log(data.data[i].name);
 //       // get venue name
-      $('#venue').text(data.data[i].location.name);
+      console.log(data.data[i].location.name);
 //       //get band image (if available)
       let bandImage = document.getElementById("band-img");
       bandImage.setAttribute("src", data.data[i].image); //sets top img of card to whatever image was given from api for current artist/band
@@ -60,46 +60,24 @@ function getShows(artistName) { //when function called, will be passed the artis
     
 };
 
-// var granimInstance = new Granim({
-//   element: '#canvas-image-blending',
-//   direction: 'top-bottom',
-//   isPausedWhenNotInView: true,
-//   image : {
-//       source: 'assets/groupiehero_720.png',
-//       blendingMode: 'multiply',
-//       stretchMode: ['stretch-if-bigger','none']
-//   },
-//   states : {
-//       "default-state": {
-//           gradients: [
-//               ['#29323c', '#485563'],
-//               ['#FF6B6B', '#556270'],
-//               ['#80d3fe', '#7ea0c4'],
-//               ['#f0ab51', '#eceba3']
-//           ],
-//           transitionSpeed: 7000
-//       }
-//   }
-// });
-
-const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-    
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar',
-      },
-    });
+var granimInstance = new Granim({
+  element: '#canvas-image-blending',
+  direction: 'top-bottom',
+  isPausedWhenNotInView: true,
+  image : {
+      source: 'assets/groupiehero_720.png',
+      blendingMode: 'multiply',
+      stretchMode: ['stretch-if-bigger','none']
+  },
+  states : {
+      "default-state": {
+          gradients: [
+              ['#29323c', '#485563'],
+              ['#FF6B6B', '#556270'],
+              ['#80d3fe', '#7ea0c4'],
+              ['#f0ab51', '#eceba3']
+          ],
+          transitionSpeed: 7000
+      }
+  }
+});
