c30a69a49cc78fb29ecef60560051623303b0bc2
smulch <mulcahy.samuel@gmail.com>
2023-02-24 10:54:14 -0500

    added base for secondary carousel

--- assets/script.js
+++ assets/script.js
@@ -90,3 +90,25 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
       },
     });
+
+    const swiper2 = new Swiper('.swiper2', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+    
+      // If we need pagination
+      // pagination: {
+      //   el: '.swiper-pagination',
+      // },
+    
+      // Navigation arrows
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+    
+      // And if we need scrollbar
+      // scrollbar: {
+      //   el: '.swiper-scrollbar',
+      // },
+    });
